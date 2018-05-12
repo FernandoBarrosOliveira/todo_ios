da133efec4dd2e8a5cbc784494e64e63456de470
Mark Gomes <ellyosmark@gmail.com>
2018-05-12 10:24:22 -0300

    Ajustes no storyboard

--- TODO_iOS/ViewController.swift
+++ TODO_iOS/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func btnLoginClick(_ sender: Any) {
+    }
+    @IBAction func btnSignUpClick(_ sender: Any) {
+    }
 
 }
 
