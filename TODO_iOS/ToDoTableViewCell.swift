a990856f154f2a9213c5fa3c32bf34e869455965
Mark Gomes <ellyosmark@gmail.com>
2018-05-12 10:50:28 -0300

    Adicao da definição do item da lista

---
+++ TODO_iOS/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  GoogleToolboxForMac
+//
+//  Created by ios on 12/05/18.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
